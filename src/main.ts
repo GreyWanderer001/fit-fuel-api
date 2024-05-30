913e21d91cb5719160d4468d18b387aac1e6ff26
robert <roberts.shalajevs@gmail.com>
2024-05-30 14:05:58 +0300

    v3

--- src/main.ts
+++ src/main.ts
@@ -23,7 +23,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(80);
+  await app.listen(80, "0.0.0.0");
 }
 
 bootstrap().then();
